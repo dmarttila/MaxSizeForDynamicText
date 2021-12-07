2e644a276aba0e7cda7554670825657a6629adf1
Doug Marttila <dmarttila@gmail.com>
2021-12-07 08:06:04 -0500

    Initial Commit

---
+++ MaxSizeForDynamicText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaxSizeForDynamicText
+//
+//  Created by Douglas Marttila on 12/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MaxSizeForDynamicText/MaxSizeForDynamicTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxSizeForDynamicTextApp.swift
+//  MaxSizeForDynamicText
+//
+//  Created by Douglas Marttila on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MaxSizeForDynamicTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
